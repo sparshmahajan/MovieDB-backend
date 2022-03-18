8dec11ca03268a19c39cb49a00c971e50cf107d7
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-19 03:56:52 +0530

    new api's add

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const _ = require("lodash");
 
 const search = async function (req, res) {
-    const query = _.toLower(req.body.name);
+    const query = _.toLower(req.params.name);
     const response = await axios.get("https://api.themoviedb.org/3/search/multi?api_key=" + process.env.TMDB_API_KEY + "&query=" + query);
     try {
         res.json(response.data.results);
@@ -36,7 +36,7 @@ const search_tv_by_id = async function (req, res) {
     }
 };
 
-const trendig_movie = async function (req, res) {
+const trending_movie = async function (req, res) {
     const response = await axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=" + process.env.TMDB_API_KEY);
     try {
         res.json(response.data.results);
@@ -46,7 +46,7 @@ const trendig_movie = async function (req, res) {
     }
 };
 
-const trendig_tv = async function (req, res) {
+const trending_tv = async function (req, res) {
     const response = await axios.get("https://api.themoviedb.org/3/trending/tv/week?api_key=" + process.env.TMDB_API_KEY);
     try {
         res.json(response.data.results);
@@ -56,4 +56,84 @@ const trendig_tv = async function (req, res) {
     }
 };
 
-module.exports = { search, search_movie_by_id, search_tv_by_id, trendig_movie, trendig_tv };
+const popular_movie = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const popular_tv = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/tv/popular?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const top_rated_movie = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const top_rated_tv = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/tv/top_rated?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const latest_movie = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/movie/latest?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const latest_tv = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/tv/latest?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const upcoming_movie = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+const on_the_air = async function (req, res) {
+    const response = await axios.get("https://api.themoviedb.org/3/tv/on_the_air?api_key=" + process.env.TMDB_API_KEY);
+    try {
+        res.json(response.data.results);
+    } catch (error) {
+        res.send({ message: "SOMETHING WENT WRONG" });
+        console.log(error);
+    }
+};
+
+module.exports = { search, search_movie_by_id, search_tv_by_id, trending_movie, trending_tv, popular_movie, popular_tv, top_rated_movie, top_rated_tv, latest_movie, latest_tv, upcoming_movie, on_the_air };
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { search, search_movie_by_id, search_tv_by_id, trendig_movie, trendig_tv } = require('../controllers/api.controller')
+const { search, search_movie_by_id, search_tv_by_id, trending_movie, trending_tv, popular_movie, popular_tv, latest_movie, latest_tv, top_rated_movie, top_rated_tv, upcoming_movie, on_the_air } = require('../controllers/api.controller')
 
-router.get('/search', search);
+router.get('/search/:name', search);
 router.get('/search_movie/:id', search_movie_by_id);
 router.get('/search_tv/:id', search_tv_by_id);
-router.get('/trending_movie', trendig_movie);
-router.get('/trending_tv', trendig_tv);
+router.get('/trending_movie', trending_movie);
+router.get('/trending_tv', trending_tv);
+router.get('/popular_movie', popular_movie);
+router.get('/popular_tv', popular_tv);
+router.get('/latest_movie', latest_movie);
+router.get('/latest_tv', latest_tv);
+router.get('/top_rated_movie', top_rated_movie);
+router.get('/top_rated_tv', top_rated_tv);
+router.get('/upcoming_movie', upcoming_movie);
+router.get('/on_the_air', on_the_air);
 
 module.exports = router;
