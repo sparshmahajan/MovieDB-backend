304f99956c9204e05e5d6aa35176d31400b975c2
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-26 16:27:34 +0530

    cookie fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -42,7 +42,7 @@ const login = function (req, res) {
             const result = await Decrypt(password, foundUser.password);
             if (result === true) {
                 const token = getToken({ userId: foundUser._id });
-                res.cookie("token", token);
+                res.cookie("token", token, { sameSite: 'None' });
                 const { name, email, movie } = foundUser;
                 res.json({
                     name: name,
