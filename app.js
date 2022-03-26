7b0ebcb9251b0da98561238ee3cf1a4defa2c254
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-26 15:09:38 +0530

    frontend link fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://movie-db-leqs6ryz7-sparshmahajan.vercel.app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
