ac62b27cbdc1fd73b8d2f371d6c3a85c3a05e0fd
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-26 18:03:40 +0530

    fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', 'https://movie-db-brown.vercel.app');
+    res.header('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
