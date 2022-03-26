c20c4079bc9097d9ce9fb53004cee2a405915243
sparshmahajan <sparshmahajan3@gmail.com>
2022-03-26 17:58:24 +0530

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: "https://movie-db-sparshmahajan.vercel.app/" }));
+app.use(cors({ credentials: true, origin: "https://movie-db-sparshmahajan.vercel.app" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,17 +20,17 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://movie-db-sparshmahajan.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://movie-db-sparshmahajan.vercel.app');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
