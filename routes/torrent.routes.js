e558e70e1e5d8da61d7cec80c5f835307822352b
sparshmahajan <sparshmahajan3@gmail.com>
2022-04-03 16:11:45 +0530

    torrent fix

--- routes/torrent.routes.js
+++ routes/torrent.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { torrent } = require('../controllers/torrent.controller')
 
-router.get('/torrent', torrent);
+router.get('/torrent/:id', torrent);
 
 module.exports = router;
