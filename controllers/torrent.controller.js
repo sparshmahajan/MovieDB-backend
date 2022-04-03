a50eeaafa7a76884ecc12d1a883b66e3fee5d819
sparshmahajan <sparshmahajan3@gmail.com>
2022-04-03 16:08:03 +0530

    torrent fix

--- controllers/torrent.controller.js
+++ controllers/torrent.controller.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require('axios');
 
 const torrent = async (req, res) => {
-    let { showId } = req.body;
+    let { showId } = req.params.id;
     let url = `https://yts.torrentbay.to/api/v2/list_movies.json?query_term=${showId}`;
     let response = await axios.get(url);
     let torrents = response.data;
